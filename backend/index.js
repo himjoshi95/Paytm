41c65919801b710654b2e5725ea4236f1632e3a5
himjoshi95 <himanshujoshi1012@gmail.com>
2024-09-19 15:42:46 +0530

    Step 3 - Created routing file structure

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,12 @@
 const express = require("express")
-const {config} = require('dotenv')
+const { config } = require('dotenv')
+const rootRouter = require('./routes/index')
 
 config()
 const app = express()
 const PORT = process.env.PORT
+
+app.use('/api/v1',rootRouter)
 
 
 app.listen(PORT, () => {
