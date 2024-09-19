9694f509b8ad12194e2ed938ab7ecdfc0fb90345
himjoshi95 <himanshujoshi1012@gmail.com>
2024-09-19 15:55:38 +0530

    Step 5 - Added cors, body parser,bcrypt and jsonwebtoken

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,12 @@
 const express = require("express")
 const { config } = require('dotenv')
 const rootRouter = require('./routes/index')
+const cors = require('cors')
 
 config()
 const app = express()
+app.use(express.json())
+app.use(cors())
 const PORT = process.env.PORT
 
 app.use('/api/v1',rootRouter)
